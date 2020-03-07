92fe92c8159b8c528098b62550da6d39098e8015
Patryk Syc <patryk@edefine.pl>
2020-03-07 16:11:04 +0100

    Initial commit

---
+++ sudoku.py
@@ -0,0 +1,88 @@
+from numpy import array
+from typing import Generator, List
+
+SudokuGrid = List[List[int]]
+
+
+class SudokuSolver:
+    def solve(self, sudoku: SudokuGrid) -> Generator[SudokuGrid, None, None]:
+        for y in range(9):
+            for x in range(9):
+                if not sudoku[y][x]:
+                    for value in range(1, 10):
+                        if self.__is_valid_move(sudoku, x, y, value):
+                            sudoku[y][x] = value
+                            yield from self.solve(sudoku)
+                            sudoku[y][x] = 0
+
+                    return None
+
+        yield sudoku
+
+    def __is_valid_move(self, sudoku: SudokuGrid, x: int, y: int, value: int) -> bool:
+        for i in range(9):
+            if sudoku[y][i] == value or sudoku[i][x] == value:
+                return False
+
+        x0 = (x // 3) * 3
+        y0 = (y // 3) * 3
+        for j in range(y0, y0 + 3):
+            for i in range(x0, x0 + 3):
+                if sudoku[j][i] == value:
+                    return False
+
+        return True
+
+
+if __name__ == '__main__':
+    hard = [
+        [8, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 3, 6, 0, 0, 0, 0, 0],
+        [0, 7, 0, 0, 9, 0, 2, 0, 0],
+        [0, 5, 0, 0, 0, 7, 0, 0, 0],
+        [0, 0, 0, 0, 4, 5, 7, 0, 0],
+        [0, 0, 0, 1, 0, 0, 0, 3, 0],
+        [0, 0, 1, 0, 0, 0, 0, 6, 8],
+        [0, 0, 8, 5, 0, 0, 0, 1, 0],
+        [0, 9, 0, 0, 0, 0, 4, 0, 0],
+    ]
+
+    easy_single = [
+        [5, 3, 0, 0, 7, 0, 0, 0, 0],
+        [6, 0, 0, 1, 9, 5, 0, 0, 0],
+        [0, 9, 8, 0, 0, 0, 0, 6, 0],
+        [8, 0, 0, 0, 6, 0, 0, 0, 3],
+        [4, 0, 0, 8, 0, 3, 0, 0, 1],
+        [7, 0, 0, 0, 2, 0, 0, 0, 6],
+        [0, 6, 0, 0, 0, 0, 2, 8, 0],
+        [0, 0, 0, 4, 1, 9, 0, 0, 5],
+        [0, 0, 0, 0, 8, 0, 0, 7, 9],
+    ]
+
+    easy_multiple = [
+        [5, 3, 0, 0, 7, 0, 0, 0, 0],
+        [6, 0, 0, 1, 9, 5, 0, 0, 0],
+        [0, 9, 8, 0, 0, 0, 0, 6, 0],
+        [8, 0, 0, 0, 6, 0, 0, 0, 3],
+        [4, 0, 0, 8, 0, 3, 0, 0, 1],
+        [7, 0, 0, 0, 2, 0, 0, 0, 6],
+        [0, 6, 0, 0, 0, 0, 2, 8, 0],
+        [0, 0, 0, 4, 1, 9, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    ]
+
+    empty = [
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    ]
+
+    solver = SudokuSolver()
+    for solution in solver.solve(easy_multiple):
+        print(array(solution))
